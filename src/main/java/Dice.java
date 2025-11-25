bae8619e5c59b1c1f2ff084106dbc32586a435e6
Bogdan Sorinescu <bogdansorinescu@gmail.com>
2025-11-25 14:09:46 +0100

    Passed test for dice(GREEN)

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -2,6 +2,7 @@ public class Dice {
 
     private final int sides;
 
+    //Green
     public Dice(int sides){
         if (sides==1){
             throw new IllegalArgumentException("Dice must have at least 2 sides");
