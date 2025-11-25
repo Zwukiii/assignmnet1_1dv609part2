10a27f921e5fa54afdada922577d0b407dab789d
as228vx <as228vx@student.lnu.se>
2025-11-25 14:12:29 +0100

    Test failed on shoulÃdStarttScoreAtZero

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerTest {
+
+    Player sut;
+
+
+    @BeforeEach
+    void setup() {
+        sut = new Player("Alen");
+    }
+
+    @Test
+    void shouldStartScoreAtZero() {
+        assertEquals(0, sut.getScore());
+    }
+}
