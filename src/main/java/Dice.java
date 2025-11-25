831e318267ee6548f97256a5654827c831305864
Bogdan Sorinescu <bogdansorinescu@gmail.com>
2025-11-25 14:06:01 +0100

    Passed test for dice(GREEN)

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -4,7 +4,7 @@ public class Dice {
 
     public Dice(int sides){
         if (sides==1){
-            System.out.println("Invalid dice");
+            throw new IllegalArgumentException("Dice must have at least 2 sides");
         }
         this.sides= sides;
     }
