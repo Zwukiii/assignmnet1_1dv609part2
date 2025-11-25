73719a1aab21da9d96a484c06cd4397f19ae66a2
Bogdan Sorinescu <bogdansorinescu@gmail.com>
2025-11-25 14:22:45 +0100

    Made a small change in code quality(REFACTORED)

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -3,10 +3,12 @@ public class Dice {
     private final int sides;
 
     //Refactor
-    public Dice(int sides){
+    public Dice(int sides) throws IllegalArgumentException{
         if (sides<2){
             throw new IllegalArgumentException("Dice must have at least 2 sides");
         }
         this.sides= sides;
     }
+
+
 }
